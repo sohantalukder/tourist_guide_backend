967493e6e4a2ff27e7f5ed6739739af56bcdeb3c
sohantalukder <mdtalukder.sohan@gmail.com>
2023-03-08 23:09:00 +0600

    create two utilites file generate web token and generate response

---
+++ utlis/generateToken.js
@@ -0,0 +1,10 @@
+COOKIE_EXPIRE;
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
+
+export default generateToken;
