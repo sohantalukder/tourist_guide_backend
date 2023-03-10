c47a69a10cf7068f732d06dfae54d8026a5e364a
sohantalukder <mdtalukder.sohan@gmail.com>
2023-03-10 11:51:03 +0600

    create admin login api

--- controllers/Admin/adminController.js
+++ controllers/Admin/adminController.js
@@ -1,37 +1,54 @@
 import User from "../../Models/User/userModel.js";
 import asyncHandler from "express-async-handler";
 import { response } from "../../utlis/generateResponse.js";
+import generateToken from "../../utlis/generateToken.js";
 // Register a User
-const adminLogin = async (req, res, next) => {
-    const user = req.body;
-    const requester = req.decodedEmail;
-    if (requester) {
-        const requesterAccount = await User.findOne({
-            email: requester,
-        });
-        if (requesterAccount.role === "admin") {
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: "admin" } };
-            const result = await User.updateOne(filter, updateDoc);
-            res.json(result);
+const adminLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (user && (await user.comparePassword(password))) {
+        if (user.role === "admin") {
+            res.status(200).json(
+                response({
+                    code: 200,
+                    message: "Ok",
+                    records: {
+                        records: {
+                            id: user._id,
+                            name: user.name,
+                            email: user.email,
+                            emailVerify: user.emailVerify,
+                            fvtFoods: user.fvtFoods,
+                            fvtPlace: user.fvtPlace,
+                            status: user.status,
+                            role: user.role,
+                            image: user.image,
+                            location: user.location,
+                            contactNumber: user.contactNumber,
+                            description: user.description,
+                            token: generateToken(user._id),
+                        },
+                    },
+                })
+            );
+        } else {
+            res.status(401).json(
+                response({
+                    code: 401,
+                    message: "You do not have permission to make admin",
+                })
+            );
         }
     } else {
-        res.status(401).json({
-            message: "You do not have permission to make admin",
-        });
+        res.status(401).json(
+            response({
+                code: 401,
+                message: "Invalid email or password",
+            })
+        );
     }
-};
+});
 
-const admin = async (req, res, next) => {
-    const email = req.params.email;
-    const query = { email: email };
-    const role = await User.findOne(query);
-    let isAdmin = false;
-    if (role?.role === "admin") {
-        isAdmin = true;
-    }
-    res.json({ admin: isAdmin });
-};
 const getAllUsers = asyncHandler(async (req, res) => {
     const sort = { _id: -1 };
     const users = await User.find({}).sort(sort);
@@ -50,4 +67,4 @@ const getAllUsers = asyncHandler(async (req, res) => {
     }
 });
 
-export { getAllUsers };
+export { getAllUsers, adminLogin };
--- routes/Admin/adminRoutes.js
+++ routes/Admin/adminRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllUsers } from "../../controllers/Admin/adminController.js";
+import {
+    adminLogin,
+    getAllUsers,
+} from "../../controllers/Admin/adminController.js";
 import { admin, protect } from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/allUsers").get(protect, admin, getAllUsers);
+router.route("/").post(adminLogin);
 export default router;
