3bc0f123f7d1580bd450fc9225a83a2f7b3fc2ff
sohantalukder <mdtalukder.sohan@gmail.com>
2023-03-20 23:10:18 +0600

    create schema for add division, districts and thana

---
+++ Models/Locations/Districts/Upazilas/upazilas.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Upazilas = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    division_code: {
+        type: Number,
+        required: true,
+    },
+    district_code: {
+        type: Number,
+        required: true,
+    },
+    postalCode: {
+        type: Number,
+        required: true,
+    },
+    geocode: {
+        type: String,
+        required: false,
+        default: null,
+    },
+    createdAt: Date,
+    expiredAt: Date,
+});
+
+export default Upazilas;
